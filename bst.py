9a8bd35d63c7586e8952af1ee7ced2fcba9a4a1d
Victor Gomes <victormenezez15@gmail.com>
2018-10-21 00:39:30 -0300

    Improvements in the insertion structure. Parent, left_nodes, and right_nodes fields added for better implementation of functions. Insert method already changed

--- bst.py
+++ bst.py
@@ -4,23 +4,27 @@ class Node:
         self.left = None
         self.right = None
         self.data = key
+        self.left_nodes = 0
+        self.right_nodes = 0
+        self.parent = None
 
 
 #inserts a value into tree
 def insert(root, node):
-    if root is None:
-        root = node;
-    else:
-        if root.data < node.data:
-            if root.right is None:
-                root.right = node
-            else:
-                insert(root.right, node)
+    if root.data < node.data:
+        if root.right is None:
+            root.right = node
+            node.parent = root
         else:
-                if root.left is None:
-                    root.left = node
-                else:
-                    insert(root.left, node)
+            insert(root.right, node)
+        root.right_nodes += 1
+    else:
+        if root.left is None:
+            root.left = node
+            node.parent = root
+        else:
+            insert(root.left, node)
+        root.left_nodes += 1
 
 
 #in order traversal
@@ -51,7 +55,6 @@ def enesimo_elemento(root, index):
     current = root
     s = []
     done = 0
-    index_cont = 0
     aux = None
 
     while not done and index > 0:
@@ -69,23 +72,38 @@ def enesimo_elemento(root, index):
 
     return aux.data
 
-#returns the index of element informed
-def posicao(root, position):
-        current = root
-        s = []
-        done = 0
 
-        while(not done):
-            if current is not None:
-                s.append(current)
-                current = current.left
+#returns the index of value informed
+def posicao(root, value):
+    current = root
+    s = []
+    done = 0
+    index = 1
+
+    while not done:
+        if current is not None:
+            s.append(current)
+            current = current.left
+        else:
+            if(len(s) > 0):
+                current = s.pop()
+
+                if current.data == value:
+                    return index
+
+                index += 1
+                current = current.right
             else:
-                if(len(s) > 0):
-                    current = s.pop()
-                    current = current.right
-                else:
-                    done = 1
+                done = 1
 
+
+def nodes_count(root):
+    pass
+
+
+#no comments at all
+def mediana(root):
+    pass
 
 if __name__ == "__main__":
     root = Node(10)
@@ -93,9 +111,17 @@ if __name__ == "__main__":
     insert(root, Node(30))
     insert(root, Node(20))
     insert(root, Node(40))
-    insert(root, Node(5))
-    insert(root, Node(4))
+    insert(root, Node(19))
+    insert(root, Node(21))
+    insert(root, Node(39))
+    insert(root, Node(41))
     insert(root, Node(6))
+    insert(root, Node(4))
+    insert(root, Node(8))
+    insert(root, Node(7))
+    insert(root, Node(9))
+    insert(root, Node(3))
+    # insert(root, Node(5))
 
     # print(search(root, 3))
     # print(search(root, 4))
@@ -103,5 +129,5 @@ if __name__ == "__main__":
 
     inorder(root)
     print(enesimo_elemento(root, 3))
-    print(enesimo_elemento(root, 2))
-    print(enesimo_elemento(root, 1))
+    print(posicao(root, 4))
+    print(root.right_nodes)
